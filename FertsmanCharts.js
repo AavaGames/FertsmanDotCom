8891836e71e20d6995e487b9bccd0c7809ffa2c0
AavaGames <keylogicgames@gmail.com>
2020-11-25 22:57:08 -0400

    Split JS into separate files

---
+++ FertsmanCharts.js
@@ -0,0 +1,41 @@
+var DEBUG = false;
+
+var sheets_api_key = "AIzaSyBCKARaVh3Ho0f5NEdAfvzTi5_U-UgkNLM"
+
+//'use strict';
+
+var lineOptions = {
+    colors: ["rgba(255, 255, 255, 1)",
+        "rgba(0, 175, 181, 1)",
+        "rgba(255, 119, 0, 1)",
+        "rgba(163, 0, 0, 1)",
+        "rgba(0, 71, 119, 1)",
+        "rgba(239, 210, 141, 1)"]
+}
+
+var chartDefaultColor = "rgba(0, 0, 0, 0.1)";
+var chartFontFamily = "Open Sans";
+
+function FormatLink(spreadSheetID, ...ranges) {
+    var link;
+    const startOfLink = "https://sheets.googleapis.com/v4/spreadsheets/";
+    const forceColumns = "&majorDimension=COLUMNS"
+    const apiKey = "&key=" + sheets_api_key;
+
+    var range = "";
+    ranges.forEach(element => {
+        if (range == "") {
+            range = "/values:batchGet?ranges=" + element;
+        }
+        else {
+            range = range + "&ranges=" + element;
+        }
+    });
+
+    link = startOfLink + spreadSheetID + range + forceColumns + apiKey;
+
+    if (DEBUG)
+        console.log(link);
+
+    return link;
+}
