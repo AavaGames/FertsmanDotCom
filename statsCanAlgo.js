de997f0512eeb7de745bc07056dd489d7ef538f2
AavaGames <keylogicgames@gmail.com>
2020-12-07 16:03:35 -0400

    GetLongestColumn, FillOutRanges

--- statsCanAlgo.js
+++ statsCanAlgo.js
@@ -21,8 +21,9 @@ function promptUserForInput(promptText) {
 
 //Imports a CSV file at a URL into the Google Sheet
 function importCsvFromUrl() {
-    //var table = 36100434;
-    var table = promptUserForInput("Please enter table number");
+    //var table = 10100107;
+    var table = 36100434;
+    //var table = promptUserForInput("Please enter table number");
     var url = ("https://www150.statcan.gc.ca/n1/tbl/csv/" + table + "-eng.zip");
 
     displayToastAlert("Fetching");
@@ -42,42 +43,108 @@ function importCsvFromUrl() {
 
     var data = SortStatsCanData(contents);
 
-    var maxLength = 25;
+    //data = FillOutRange(data);
 
-    var splitData = SplitArrayIntoArrays(data, maxLength);
+    //var maxLength = 25;
+    //var splitData = SplitArrayIntoArrays(data, maxLength);
 
-    ClearSheet();
+    //ClearSheet();
 
-    for (var i = 0; i < splitData.length; i++)
-    {
-        displayToastAlert("Writing part " + (i + 1) + " / " + splitData.length + " to sheet");
-        // Starting at row 2 bypasses a Spreadsheet Error bug
-        WriteDataToSheet(splitData[i], 2 + (maxLength * i));
-    }
+    //for (var i = 0; i < splitData.length; i++)
+    // for (var i = 0; i < 4; i++)
+    // {
+    //     displayToastAlert("Writing part " + (i + 1) + " / " + splitData.length + " to sheet");
+
+    //     console.log("Writing part " + (i + 1) + " / " + splitData.length + " to sheet")
+    //     // Starting at row 2 bypasses a Spreadsheet Error bug
+    //     WriteDataToSheet(splitData[i], 2 + (maxLength * i));
+
+    //     console.log("Finished writing part " + (i + 1));
+    // }
     // Delete empty first row
-    sheet.deleteRow(1);
+
+    WriteDataToSheet(data, 1);
 
     displayToastAlert("The CSV file was successfully imported");
 }
 
-var spreadSheetName = "STATISTICS CANADA TEST";
+function GetLongestLengthColumn(array)
+{
+    var length = array[0].length;
+
+    for (var i = 1; i < array.length; i++)
+    {
+        length = length < array[i].length ? array[i].length : length;
+    }
+
+    console.log("0 length " + array[0].length + " - longest length " + length);
+
+    return length;
+}
+
+var spreadSheetName = "ScriptTest";
 
 //Writes a 2D array of data into existing sheet
 function WriteDataToSheet(_data, startRow) {
+    displayToastAlert("Writing to sheet");
+    console.log("Writing to sheet");
+
+    //var longestColumn = GetLongestLengthColumn(_data);
     var ss = SpreadsheetApp.getActive();
 
-    // OOIJOIJOIJOIJ Change this to current sheet or get sheet
+    // TO DO Change this to current sheet or get sheet
 
-    sheet = ss.getSheetByName(spreadSheetName);
-    sheet.getRange(startRow, 1, _data.length, _data[0].length).setValues(_data);
+    var sheet = ss.getSheetByName(spreadSheetName);
+    var ranges = sheet.getRange(startRow, 1, _data.length, _data[0].length)
+    //ranges.setValues(_data);
+
+    // Based on https://developers.google.com/apps-script/advanced/sheets
+    var request = {
+        'valueInputOption': 'USER_ENTERED',
+        'data': [
+        {
+            'range': spreadSheetName + "!" + ranges.getA1Notation(),
+            'majorDimension': 'ROWS',
+            'values': _data
+        }]
+    };
+    Sheets.Spreadsheets.Values.batchUpdate(request, SpreadsheetApp.getActiveSpreadsheet().getId());
+    var sheetID = sheet.getSheetId();
+    //Sheets.Spreadsheets.Values.batchUpdate(request, sheetID);
+    console.log('done writing through api');
+    //sheet.deleteRow(1);
 }
 
 function ClearSheet()
 {
+    displayToastAlert("Clearing Sheet");
+
     var ss = SpreadsheetApp.getActive();
     sheet = ss.getSheetByName(spreadSheetName);
     sheet.clear();
 }
+
+function FillOutRange(range, fillItem) {
+    var fill = (fillItem === undefined) ? "" : fillItem;
+
+    //Get the max row length out of all rows in range.
+    var initialValue = 0;
+    var maxRowLen = range.reduce(function (acc, cur) {
+        return Math.max(acc, cur.length);
+    }, initialValue);
+
+    //Fill shorter rows to match max with selecte value.
+    var filled = range.map(function (row) {
+        var dif = maxRowLen - row.length;
+        if (dif > 0) {
+            var arizzle = [];
+            for (var i = 0; i < dif; i++) { arizzle[i] = fill };
+            row = row.concat(arizzle);
+        }
+        return row;
+    })
+    return filled;
+};
 
 function SplitArrayIntoArrays(data, maxLength) {
     var newData = new Array();
