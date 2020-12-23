02ecc3c3ad3208bec60105f662989541c5a29160
Aavagames <keylogicgames@gmail.com>
2020-12-22 20:36:39 -0400

    Almost finished YoY converter script

--- GoogleScripts/YoY.js
+++ GoogleScripts/YoY.js
@@ -1,33 +1,37 @@
-function ConvertValuesToYoY(oldData) {
+function ConvertValuesToYoY(data) {
     // Data Structure
     // oldData[columns][rows] 
     // newData[columns][rows]
-    var newData = [[]];
+    const oldData = data;
+    var newData = oldData;
 
     // YoY % = (This Year - Last Year) / Last Year
 
     var lastDatesIndexs = [];
     var currentDatesIndexs = [];
 
-    // Set Date column
-    newData[0] = oldData[0];
+    var amountOfColumns = oldData.length;
+    var amountOfRows = oldData[0].length;
 
-    for (var column = 1; column < oldData[0].length; column++)
+    for (var column = 1; column < amountOfColumns; column++)
     {
-        // Set Header Cell
-        newData[column][0] = oldData[column][0];
-
-        for (var row = 1; row < oldData[0][0].length; row++)
+        // Skip first year of data
+        for (var row = 14; row < amountOfRows; row++)
         {
             // Count to 12 then reset for dates
+            var lastValue = oldData[column][row - 12];
+            var currentValue = oldData[column][row];
+            var yoy = Number((currentValue - lastValue) / lastValue);
+            // rounds value
+            yoy = +yoy.toFixed(5);
+            newData[column][row] = yoy;
 
-            // Place all last dates index's into array
-            // Place all current dates index's into array
-            // convert values using formula
-            // when finished set currentDates = lastDates
+            console.log(lastValue, currentValue, yoy);
         }
+        break;
+        // remove first 12 data rows
     }
-    
+    //console.log(newData);
 
     return newData;
 }
