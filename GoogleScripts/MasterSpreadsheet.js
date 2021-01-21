2b213ed62771d29d345f3ae7479ed9756f0e2082
Aavagames <keylogicgames@gmail.com>
2021-01-21 14:19:33 -0400

    Wrote pseudocode for Master Spreadsheet

--- GoogleScripts/MasterSpreadsheet.js
+++ GoogleScripts/MasterSpreadsheet.js
@@ -1,4 +1,61 @@
 // Give sheet name
-function PullDataFromSheet(sheet) {
-    // Pull data from sheet_dictionary
+function PullDataForSheet(sheet) {
+    // Pull data from sheet_Input with Columns as major dimension
+    // Delete first / header row of all columns
+    var inputData = [[]];
+
+    // Pull first column from sheet_Data for dates, if new month hasn't been added yet, add it.
+    var masterSheetDates = [];
+
+    var spreadsheetID;
+    var sheetName;
+    var headers = [];
+    var funcParameters = [];
+
+    var data = [[]];
+
+    while (true)
+    {
+        // Move down first column until a spreadsheetID is gotten,
+        // then move to the second column for sheet name,
+        // then go to third column for header name(s)
+        // continue until bumping into another spreadsheetID or sheet name
+        
+        if (String(sheetName).includes("func_"))
+        {
+            if (sheetName == "func_END")// || reached end of sheet)
+                break;
+
+            UseSheetFunction(sheetName, header[0], funcParameters);
+        }
+
+        // grab data from spreadsheet
+        // look for headers in spreadsheet
+        // once gotten add to sheet in appropriate date row
+        //   get earliest date of spreadsheet, find date in new spreadsheet, splice into that row
+
+        var sheetData = await PullDataFromSheet();
+        var headerData = [[]];
+        // Find headers in sheet
+
+        // Find proper date index
+        var dateIndex;
+        // Add headerData to data in proper date position
+        for (var i = 0; i < headerData.length; i++)
+        {
+            data.push(new Array());
+            // in last data column
+            data.splice(dateIndex, 0, headerData[i]);
+        }
+    }
+
+    // Write data to sheet
+    WriteDataToSheet(data, sheet + "_Data");
+}
+
+function PullDataFromSheet(spreadsheetID, sheetName, ...headers)
+{
+    var sheetData = [[]];
+
+    return sheetData;
 }
---
+++ GoogleScripts/MasterSpreadsheetFunctions.js
@@ -0,0 +1,43 @@
+const SheetFunctionTypes = Object.freeze({
+    "Add": 1, 
+    "Subtract": 2, 
+    "Total": 3, 
+    "Average": 4,
+    "Percent": 5,
+    "Growing": 6,
+    "Shrinking": 7,
+    "Ratio": 8,
+    "RatioPercent": 9,
+    enumLength: 9
+});
+
+// Will need a YoY converter for some functions
+
+function UseSheetFunction(funcName, header, funcParameters)
+{
+    funcName = String(funcName).toLowerCase();
+    var funcToRun = FindFunctionToRun(funcName);
+
+    switch (funcToRun) {
+        case SheetFunctionTypes.Add:
+            SheetFunctionAdd(funcParameters)
+            break;
+
+        default:
+            console.log("WARNING: Could not find function " + funcName);
+            break;
+    }
+}
+
+function FindFunctionToRun(funcName)
+{
+    switch (funcName) {
+        case SheetFunctionTypes.Add:
+            if (funcName == "add")
+                return SheetFunctionTypes.Add;
+            break;
+
+        default:
+            return null;
+    }
+}
